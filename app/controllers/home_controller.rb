297d433c56cae5cef14a6ddc52d4237067f7fb85
Swapnil Jagtap <developedbysj@gmail.com>
2021-01-31 15:25:46 +0530

    Added index method to home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   before_action :ensure_user_logged_in, only: [:dashboard]
 
+  def index
+    render "home/index", :locals => { menu_items: MenuItem.get_active }
+  end
+
   def login
     render "users/login", :layout => "form_layout"
   end
